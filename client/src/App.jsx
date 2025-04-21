1880d51248b85dfb9d4cd21408c44a0a397e580e
Kacper Wasiak <wasiak.kacper11@gmail.com>
2025-04-21 22:21:03 +0200

    feat(Contact.jsx): Add contact section and style it.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import Home from "./Pages/Home.jsx";
 import Gastronomy from "./Pages/Gastronomy.jsx";
 import Cart from "./Pages/Cart.jsx";
 import About from "./Pages/About.jsx";
+import Contact from "./Pages/Contact.jsx";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import "./App.css";
@@ -15,6 +16,7 @@ function App() {
           <Route path="/gastronomy" element={<Gastronomy />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </main>
---
+++ client/src/Pages/Contact.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Navbar from "../Components/Navbar.jsx";
+
+const Contact = () => {
+  return (
+    <article>
+      <Navbar name="Contact" />
+      <h1 className="pt-45 text-[10vw] md:text-[8vw] lg:text-[6vw] font-medium text-center">
+        Kontakt
+      </h1>
+      <section className="flex flex-col w-[90%] h-[100%] lg:h-[32vw] mx-auto lg:flex-row justify-center items-center gap-10 *:flex *:flex-col *:justify-evenly *:items-center *:bg-(--background) *:w-[100%] *:h-[100%] lg:*:w-[30%] *:text-center *:p-5 *:rounded-3xl">
+        <div>
+          <i className="icon-phone text-[8vw] md:text-[7vw] lg:text-[4vw]"></i>
+          <p className="text-[6vw] md:text-[5vw] lg:text-[2.5vw]">
+            000-000-000
+          </p>
+        </div>
+        <div>
+          <i className="icon-mail-alt text-[8vw] md:text-[7vw] lg:text-[4vw]"></i>
+          <p className="text-[6vw] md:text-[5vw] lg:text-[1.5vw]">
+            kontakt@fabrykawarzyw.pl
+          </p>
+        </div>
+        <div>
+          <i className="icon-location text-[8vw] md:text-[7vw] lg:text-[4vw]"></i>
+          <iframe
+            width="400"
+            height="200"
+            frameborder="0"
+            scrolling="no"
+            marginheight="0"
+            marginwidth="0"
+            className="mx-auto w-[100%] rounded-2xl"
+            src="https://maps.google.com/maps?width=400&amp;height=200&amp;hl=en&amp;q=Cedry%204,%2091-129%20%C5%81%C3%B3d%C5%BA+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+          >
+            <a href="https://www.gps.ie/collections/personal-trackers/">
+              real-time gps tracker,
+            </a>
+          </iframe>
+          <p className="text-[6vw] md:text-[5vw] lg:text-[2vw]">
+            Cedry 4, 91-129 Łódź
+          </p>
+        </div>
+      </section>
+    </article>
+  );
+};
+
+export default Contact;
