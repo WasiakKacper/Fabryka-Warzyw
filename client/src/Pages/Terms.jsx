5fb4549fdb6290630ffe518bfdc256612421bd73
Kacper Wasiak <wasiak.kacper11@gmail.com>
2025-05-23 05:54:11 +0200

    feat(Terms.jsx): Add content to terms section.

--- client/src/Pages/Terms.jsx
+++ client/src/Pages/Terms.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const Terms = () => {
   return (
-    <main className="w-100% min-h-[100vw] h-[100%] pt-45">
-      <h1>Regulamin</h1>
-      <p>
+    <main className="w-100% min-h-[100vw] h-[100%] pt-45 text-(--white)">
+      <h1 className="pt-45 text-[10vw] md:text-[8vw] lg:text-[6vw] font-medium text-center">
+        Regulamin
+      </h1>
+      <p className=" w-[60%] mt-2 mx-auto text-[3vw] md:text-[2vw] lg:text-[1.5vw]">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
         nulla in quis! Iure, unde porro! Maiores maxime corporis, culpa quasi
         voluptas voluptate ipsam officiis quis nisi, consectetur enim debitis
@@ -15,8 +17,10 @@ const Terms = () => {
         at deleniti ullam, enim libero repellendus debitis quis perspiciatis
         doloremque laboriosam quae facilis, veritatis esse in ut ex! Iste!
       </p>
-      <h1>Polityka prywatnosci</h1>
-      <p>
+      <h1 className="pt-45 text-[10vw] md:text-[8vw] lg:text-[6vw] font-medium text-center">
+        Polityka przywatnosci
+      </h1>
+      <p className=" w-[60%] mt-2 mx-auto text-[3vw] md:text-[2vw] lg:text-[1.5vw]">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
         nulla in quis! Iure, unde porro! Maiores maxime corporis, culpa quasi
         voluptas voluptate ipsam officiis quis nisi, consectetur enim debitis
