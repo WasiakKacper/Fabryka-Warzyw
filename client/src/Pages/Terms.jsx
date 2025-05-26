8f004f09225af6a2ff2b9e4ae3337e4312d405a3
Kacper Wasiak <wasiak.kacper11@gmail.com>
2025-05-26 09:22:55 +0200

    feat(Terms.jsx): Add terms and Privacy Policy.

--- client/src/Pages/Terms.jsx
+++ client/src/Pages/Terms.jsx
@@ -6,30 +6,145 @@ const Terms = () => {
       <h1 className="pt-45 text-[10vw] md:text-[8vw] lg:text-[6vw] font-medium text-center">
         Regulamin
       </h1>
-      <p className=" w-[60%] mt-2 mx-auto text-[3vw] md:text-[2vw] lg:text-[1.5vw]">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
-        nulla in quis! Iure, unde porro! Maiores maxime corporis, culpa quasi
-        voluptas voluptate ipsam officiis quis nisi, consectetur enim debitis
-        quos! Pariatur maiores esse est blanditiis ullam rem quibusdam deleniti
-        cum doloremque saepe amet dolores officiis ratione enim odit, sunt,
-        illum, impedit omnis. Unde, itaque cum quidem reiciendis animi nisi nam?
-        Dolores officia blanditiis a harum rem nemo maxime sunt. Cupiditate quia
-        at deleniti ullam, enim libero repellendus debitis quis perspiciatis
-        doloremque laboriosam quae facilis, veritatis esse in ut ex! Iste!
+      <p className=" w-[80%] mt-2 mx-auto text-[4vw] md:text-[3vw] lg:text-[1.5vw]">
+        <h3 className="text-[5vw] md:text-[4vw] lg:text-[3vw] text-center">
+          1. Postanowienia ogólne
+        </h3>
+        Niniejszy regulamin określa zasady korzystania ze sklepu internetowego
+        dostępnego pod adresem [https://fabrykawarzyw.pl]. Sklep internetowy
+        prowadzony jest przez Fabryka Warzyw Karolina Olejniczak, z siedzibą
+        przy ul. Belwederska 42, 99-100 Łęczyca, NIP: 5070080610, e-mail:
+        kontakt@fabrykawarzyw.pl. Regulamin jest nieprzerwanie dostępny na
+        stronie internetowej sklepu i umożliwia każdemu użytkownikowi jego
+        pozyskanie, odtwarzanie i utrwalanie treści. Dokonując zakupu w sklepie,
+        klient akceptuje postanowienia niniejszego regulaminu.
+        <h3 className="text-[5vw] md:text-[4vw] lg:text-[3vw] text-center">
+          2. Definicje
+        </h3>
+        Sprzedawca – właściciel sklepu internetowego, tj. Fabryka Warzyw
+        Karolina Olejniczak. Klient – osoba fizyczna, osoba prawna lub jednostka
+        organizacyjna nieposiadająca osobowości prawnej, która korzysta ze
+        sklepu internetowego. Konsument – osoba fizyczna dokonująca zakupu
+        niezwiązanego bezpośrednio z jej działalnością gospodarczą lub zawodową.
+        Produkt – rzecz ruchoma dostępna w sklepie internetowym, będąca
+        przedmiotem umowy sprzedaży między klientem a sprzedawcą. Zamówienie –
+        oświadczenie woli klienta zmierzające do zawarcia umowy sprzedaży,
+        określające w szczególności rodzaj i liczbę zamawianych produktów.
+        <h3 className="text-[5vw] md:text-[4vw] lg:text-[3vw] text-center">
+          3. Zasady składania zamówień
+        </h3>
+        Sklep prowadzi sprzedaż na terenie Polski. Zamówienia można składać za
+        pośrednictwem strony internetowej sklepu 24 godziny na dobę, 7 dni w
+        tygodniu. W celu złożenia zamówienia należy: wybrać produkt i dodać go
+        do koszyka, wypełnić formularz zamówienia, potwierdzić zamówienie
+        przyciskiem „Zamawiam”. Złożenie zamówienia oznacza zawarcie umowy
+        sprzedaży.{" "}
+        <h3 className="text-[5vw] md:text-[4vw] lg:text-[3vw] text-center">
+          4. Ceny i płatności
+        </h3>
+        Wszystkie ceny podane w sklepie są cenami brutto (zawierają podatek VAT)
+        i wyrażone są w złotych polskich (PLN). Do każdego zamówienia dołączany
+        jest dowód zakupu – paragon lub faktura. Klient może wybrać jedną z
+        dostępnych form płatności: gotówką przy odbiorze, BLIK-iem przy
+        odbiorze. Płatność następuje w momencie dostarczenia zamówienia przez
+        sprzedawcę.
+        <h3 className="text-[5vw] md:text-[4vw] lg:text-[3vw] text-center">
+          5. Dostawa
+        </h3>
+        Zamówione produkty dostarczane są przez sprzedawcę osobiście pod
+        wskazany adres. Koszty dostawy są ustalane indywidualnie i podawane
+        klientowi przed zatwierdzeniem zamówienia. Termin dostawy ustalany jest
+        indywidualnie z klientem.
+        <h3 className="text-[5vw] md:text-[4vw] lg:text-[3vw] text-center">
+          6. Odstąpienie od umowy
+        </h3>
+        Konsument ma prawo odstąpić od umowy bez podania przyczyny w terminie 14
+        dni od otrzymania produktu. Aby skorzystać z prawa odstąpienia, należy
+        poinformować sprzedawcę o swojej decyzji drogą elektroniczną na adres
+        kontakt@fabrykawarzyw.pl lub telefonicznie. Konsument ma obowiązek
+        zwrócić produkt w stanie nienaruszonym w terminie 14 dni od złożenia
+        oświadczenia o odstąpieniu. Zwrot pieniędzy następuje nie później niż 14
+        dni od otrzymania zwróconego produktu.{" "}
+        <h3 className="text-[5vw] md:text-[4vw] lg:text-[3vw] text-center">
+          7. Reklamacje
+        </h3>
+        Sprzedawca jest odpowiedzialny względem konsumenta za zgodność towaru z
+        umową. Reklamacje można składać drogą elektroniczną na adres:
+        kontakt@fabrykawarzyw.pl. Reklamacja powinna zawierać imię i nazwisko
+        klienta, opis problemu oraz żądanie. Sprzedawca rozpatrzy reklamację w
+        terminie 14 dni.{" "}
+        <h3 className="text-[5vw] md:text-[4vw] lg:text-[3vw] text-center">
+          8. Dane osobowe
+        </h3>
+        Administratorem danych osobowych klientów jest Fabryka Warzyw Karolina
+        Olejniczak. Dane osobowe przetwarzane są w celu realizacji zamówienia i
+        zgodnie z obowiązującymi przepisami prawa. Klient ma prawo wglądu do
+        swoich danych, ich poprawiania oraz żądania ich usunięcia.{" "}
+        <h3 className="text-[5vw] md:text-[4vw] lg:text-[3vw] text-center">
+          9. Postanowienia końcowe
+        </h3>
+        W sprawach nieuregulowanych niniejszym regulaminem zastosowanie mają
+        przepisy prawa polskiego. Sprzedawca zastrzega sobie prawo do zmiany
+        regulaminu. Zmiany wchodzą w życie z chwilą ich opublikowania na stronie
+        sklepu. Zamówienia złożone przed datą zmiany regulaminu będą realizowane
+        na podstawie wersji obowiązującej w chwili składania zamówienia.
       </p>
       <h1 className="pt-45 text-[10vw] md:text-[8vw] lg:text-[6vw] font-medium text-center">
         Polityka przywatnosci
       </h1>
-      <p className=" w-[60%] mt-2 mx-auto text-[3vw] md:text-[2vw] lg:text-[1.5vw]">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
-        nulla in quis! Iure, unde porro! Maiores maxime corporis, culpa quasi
-        voluptas voluptate ipsam officiis quis nisi, consectetur enim debitis
-        quos! Pariatur maiores esse est blanditiis ullam rem quibusdam deleniti
-        cum doloremque saepe amet dolores officiis ratione enim odit, sunt,
-        illum, impedit omnis. Unde, itaque cum quidem reiciendis animi nisi nam?
-        Dolores officia blanditiis a harum rem nemo maxime sunt. Cupiditate quia
-        at deleniti ullam, enim libero repellendus debitis quis perspiciatis
-        doloremque laboriosam quae facilis, veritatis esse in ut ex! Iste!
+      <p className=" w-[80%] mt-2 mx-auto text-[3vw] md:text-[2vw] lg:text-[1.5vw]">
+        <h3 className="text-[5vw] md:text-[4vw] lg:text-[3vw] text-center">
+          1. Administrator danych osobowych
+        </h3>
+        Administratorem Twoich danych osobowych jest Fabryka Warzyw, z siedzibą
+        pod adresem: 99-100 Łęczyca Ul. Belwederska 42, NIP: 5070080610, e-mail:
+        kontakt@fabrykawarzyw.pl .
+        <h3 className="text-[5vw] md:text-[4vw] lg:text-[3vw] text-center">
+          2. Jakie dane zbieramy?
+        </h3>
+        W naszym sklepie internetowym przetwarzamy następujące dane osobowe:
+        imię i nazwisko, adres e-mail, numer telefonu, adres do dostawy (w
+        przypadku realizacji zamówienia)..{" "}
+        <h3 className="text-[5vw] md:text-[4vw] lg:text-[3vw] text-center">
+          3. Cel i podstawa przetwarzania danych
+        </h3>
+        Twoje dane przetwarzamy wyłącznie w celu: realizacji zamówień złożonych
+        w sklepie (podstawa prawna: niezbędność do wykonania umowy, art. 6 ust.
+        1 lit. b RODO), obsługi konta użytkownika i logowania do sklepu
+        (podstawa prawna: niezbędność do wykonania umowy, art. 6 ust. 1 lit. b
+        RODO), kontaktu w sprawie realizacji zamówienia lub obsługi klienta
+        <h3 className="text-[5vw] md:text-[4vw] lg:text-[3vw] text-center">
+          4. Udostępnianie danych
+        </h3>
+        Twoje dane mogą być udostępniane wyłącznie podmiotom współpracującym z
+        nami przy realizacji zamówienia (np. firmie kurierskiej) oraz podmiotom
+        świadczącym usługi IT na naszą rzecz, zawsze w zakresie niezbędnym do
+        realizacji umowy.
+        <h3 className="text-[5vw] md:text-[4vw] lg:text-[3vw] text-center">
+          5. Okres przechowywania danych
+        </h3>
+        Twoje dane będą przechowywane przez czas niezbędny do realizacji
+        zamówienia oraz przez okres wynikający z przepisów prawa, w
+        szczególności podatkowych i księgowych.
+        <h3 className="text-[5vw] md:text-[4vw] lg:text-[3vw] text-center">
+          6. Prawa użytkownika
+        </h3>
+        Masz prawo do: dostępu do swoich danych oraz ich sprostowania, usunięcia
+        danych (jeśli nie stoją na przeszkodzie względy prawne), ograniczenia
+        przetwarzania danych, wniesienia sprzeciwu wobec przetwarzania,
+        przenoszenia danych, wniesienia skargi do organu nadzorczego (Prezesa
+        Urzędu Ochrony Danych Osobowych).
+        <h3 className="text-[5vw] md:text-[4vw] lg:text-[3vw] text-center">
+          7. Cookies i technologie śledzące
+        </h3>
+        Nasz sklep nie używa plików cookies ani innych technologii śledzących,
+        jednak wykorzystujemy technologię localStorage w przeglądarce do
+        przechowywania danych sesji i preferencji użytkownika.
+        <h3 className="text-[5vw] md:text-[4vw] lg:text-[3vw] text-center">
+          8. Kontakt
+        </h3>
+        W sprawach związanych z ochroną danych osobowych możesz kontaktować się
+        z nami pod adresem e-mail: kontak@fabrykawarzyw.pl .
       </p>
     </main>
   );
