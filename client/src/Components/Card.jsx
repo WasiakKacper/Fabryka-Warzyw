c91c4c96d241c1be832e26cc218f777d9ca9f430
Kacper <139102696+WasiakKacper@users.noreply.github.com>
2025-06-04 21:33:39 +0200

    Update Card.jsx

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -67,7 +67,7 @@ const Card = (props) => {
   return (
     <div
       ref={ref}
-      className="relative w-[30%] h-full"
+      className="relative w-full lg:w-[30%] h-full"
       style={{ perspective: "1200px" }}
     >
       <div
